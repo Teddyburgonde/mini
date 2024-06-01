9a3f4f67e90d58b37644820a9df0b7bd91306418
Teddy Bandama <tebandam@2F7.42angouleme.fr>
2024-06-01 21:16:31 +0200

    refacto ok parent_process

--- src/exec/fork.c
+++ src/exec/fork.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:03:38 by tebandam          #+#    #+#             */
-/*   Updated: 2024/06/01 20:55:27 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/06/01 21:13:23 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,6 +104,42 @@ static int    child_process(t_vars *vars, t_redirection *redirect
 * dans le pipe si redirect->e_type == PIPE_OUT
 */
 
+void	close_pipe(int *pipe_fd)
+{
+	if (pipe_fd[0] != -1)
+	{
+		close(pipe_fd[0]);
+		pipe_fd[0] = -1;
+	}
+	if (pipe_fd[1] != -1)
+	{
+		close(pipe_fd[1]);
+		pipe_fd[1] = -1;
+	}
+}
+
+void	handle_pipe_closing(t_vars *vars)
+{
+	if ((vars->cmd_index - 1) % 2 == 1 && vars->nb_cmd > 1)
+	{
+		close_pipe(vars->pipe_1);
+		if (vars->pipe_2[0] != -1)
+		{
+			close(vars->pipe_2[0]);
+			vars->pipe_2[0] = -1;
+		}
+	}
+	else if ((vars->cmd_index - 1) % 2 == 0 && vars->nb_cmd > 1)
+	{
+		close_pipe(vars->pipe_2);
+		if (vars->pipe_1[0] != -1)
+		{
+			close(vars->pipe_1[0]);
+			vars->pipe_1[0] = -1;
+		}
+	}
+}
+
 int	parent_process(t_vars *vars, t_redirection *redirect, t_env **envp)
 {
 	vars->child = fork();
@@ -114,32 +150,7 @@ int	parent_process(t_vars *vars, t_redirection *redirect, t_env **envp)
 		perror("fork");
 		return (EXIT_FAILURE);
 	}
-	if ((vars->cmd_index - 1) % 2 == 1 && vars->nb_cmd > 1)
-	{
-		if (vars->pipe_1[1] != -1)
-		{
-			close(vars->pipe_1[1]);
-			vars->pipe_1[1] = -1;
-		}
-		if (vars->pipe_2[0] != -1)
-		{
-			close(vars->pipe_2[0]);
-			vars->pipe_2[0] = -1;
-		}
-	}
-	else if ((vars->cmd_index - 1) % 2 == 0 && vars->nb_cmd > 1)
-	{
-		if (vars->pipe_1[0] != -1)
-		{
-			close(vars->pipe_1[0]);
-			vars->pipe_1[0] = -1;
-		}
-		if (vars->pipe_2[1] != -1)
-		{
-			close(vars->pipe_2[1]);
-			vars->pipe_2[1] = -1;
-		}
-	}
+	handle_pipe_closing(vars);
 	if (vars->cmd_index == vars->nb_cmd)
 		vars->last_child = vars->child;
 	return (0);
