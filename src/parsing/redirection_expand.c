09fb057aef9813f2e16d95049d17c3eca2c351b7
Romain Gobet <rgobet@1G5.42angouleme.fr>
2024-06-04 14:54:10 +0200

    Rework expand redirection in progress!

--- src/parsing/redirection_expand.c
+++ src/parsing/redirection_expand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection_expand.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 13:33:03 by rgobet            #+#    #+#             */
-/*   Updated: 2024/06/04 10:16:38 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/06/04 14:50:40 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,73 +53,73 @@ int	ft_strlen_ultime(t_redirection_to_expand *tmp, t_env *env)
 	return (count);
 }
 
-t_redirection_to_expand	*expand_redirection(t_redirection_to_expand *redirection, t_env *env)
-{
-	int						i;
-	int						j;
-	char					*var_name;
-	t_redirection_to_expand	*tmp;
-	t_redirection_to_expand	*final;
-	t_env					*node;
+// t_redirection_to_expand	*expand_redirection(t_redirection_to_expand *redirection, t_env *env)
+// {
+// 	int						i;
+// 	int						j;
+// 	char					*var_name;
+// 	t_redirection_to_expand	*tmp;
+// 	t_redirection_to_expand	*final;
+// 	t_env					*node;
 
-	var_name = NULL;
-	final = NULL;
-	while (redirection)
-	{
-		i = 0;
-		j = 0;
-		tmp = lst_new_redirection_parsing_result();
-		if (!tmp)
-			return (NULL);
-		tmp->arg = malloc(sizeof(char) * ft_strlen_ultime(
-					redirection, env) + 1);
-		tmp->e_type = redirection->e_type;
-		if (ft_strcspn2(redirection->arg, "'") != 0
-			&& ft_strcspn2(&redirection->arg[i], "$") == 0
-			&& redirection->arg[i] != '$')
-		{
-			tmp->arg[i] = redirection->arg[i];
-			i++;
-			while (redirection->arg[i] && redirection->arg[i] != '\'')
-			{
-				tmp->arg[i] = redirection->arg[i];
-				i++;
-			}
-			if (redirection->arg[i])
-			{
-				tmp->arg[i] = redirection->arg[i];
-				i++;
-			}
-		}
-		else if (ft_strcspn2(&redirection->arg[i], "$") != 0 ||
-			redirection->arg[i] == '$')
-		{
-			var_name = get_var_name(&redirection->arg[ft_strcspn2(
-						&redirection->arg[i], "$")]);
-			if (lst_search_env(var_name, env))
-			{
-				node = lst_search_env(var_name, env);
-				while (env->value[j])
-				{
-					tmp->arg[i + j] = node->value[j];
-					j++;
-				}
-			}
-		}
-		else
-		{
-			while (redirection->arg[i])
-			{
-				tmp->arg[i] = redirection->arg[i];
-				i++;
-			}
-		}
-		tmp->arg[i] = 0;
-		ft_redirection_to_expand_addback(&final, tmp);
-		redirection = redirection->next;
-	}
-	return (final);
-}
+// 	var_name = NULL;
+// 	final = NULL;
+// 	while (redirection)
+// 	{
+// 		i = 0;
+// 		j = 0;
+// 		tmp = lst_new_redirection_parsing_result();
+// 		if (!tmp)
+// 			return (NULL);
+// 		tmp->arg = malloc(sizeof(char) * ft_strlen_ultime(
+// 					redirection, env) + 1);
+// 		tmp->e_type = redirection->e_type;
+// 		if (ft_strcspn2(redirection->arg, "'") != 0
+// 			&& ft_strcspn2(&redirection->arg[i], "$") == 0
+// 			&& redirection->arg[i] != '$')
+// 		{
+// 			tmp->arg[i] = redirection->arg[i];
+// 			i++;
+// 			while (redirection->arg[i] && redirection->arg[i] != '\'')
+// 			{
+// 				tmp->arg[i] = redirection->arg[i];
+// 				i++;
+// 			}
+// 			if (redirection->arg[i])
+// 			{
+// 				tmp->arg[i] = redirection->arg[i];
+// 				i++;
+// 			}
+// 		}
+// 		else if (ft_strcspn2(&redirection->arg[i], "$") != 0 ||
+// 			redirection->arg[i] == '$')
+// 		{
+// 			var_name = get_var_name(&redirection->arg[ft_strcspn2(
+// 						&redirection->arg[i], "$")]);
+// 			if (lst_search_env(var_name, env))
+// 			{
+// 				node = lst_search_env(var_name, env);
+// 				while (env->value[j])
+// 				{
+// 					tmp->arg[i + j] = node->value[j];
+// 					j++;
+// 				}
+// 			}
+// 		}
+// 		else
+// 		{
+// 			while (redirection->arg[i])
+// 			{
+// 				tmp->arg[i] = redirection->arg[i];
+// 				i++;
+// 			}
+// 		}
+// 		tmp->arg[i] = 0;
+// 		ft_redirection_to_expand_addback(&final, tmp);
+// 		redirection = redirection->next;
+// 	}
+// 	return (final);
+// }
 
 // static t_redirection_to_expand	*ft_get_last_pos(t_redirection_to_expand *lst)
 // {
@@ -132,93 +132,93 @@ t_redirection_to_expand	*expand_redirection(t_redirection_to_expand *redirection
 // 	return (NULL);
 // }
 
-static int	ft_split_redirection(t_redirection_to_expand *redirection_to_split,
-	t_redirection_to_expand **redirs, t_redirection_to_expand *origin)
-{
-	int						in;
-	t_bool					in_quote;
-	static int				i = 0;
-	t_redirection_to_expand	*tmp;
-	t_redirection_to_expand	*splitted_redirections;
+// static int	ft_split_redirection(t_redirection_to_expand *redirection_to_split,
+// 	t_redirection_to_expand **redirs, t_redirection_to_expand *origin)
+// {
+// 	int						in;
+// 	t_bool					in_quote;
+// 	static int				i = 0;
+// 	t_redirection_to_expand	*tmp;
+// 	t_redirection_to_expand	*splitted_redirections;
 
 	
-	splitted_redirections = lst_new_redirection_parsing_result();
-	if (!splitted_redirections || !redirection_to_split)
-		return (0);
-	in = 0;
-	tmp = redirection_to_split;
-	if (tmp->arg[0] == '"'
-		|| tmp->arg[0] == '\'')
-		in_quote = TRUE;
-	else
-		in_quote = FALSE;
-	while (tmp && tmp->arg[i] && in_quote == FALSE)
-	{
-		if (tmp->arg[i] == SPACE
-			|| tmp->arg[i] == TAB
-			|| tmp->arg[i] == NEW_LINE)
-			break ;
-		else if (in == 0)
-		{
-			splitted_redirections->arg = malloc(
-					ft_strcspn(&tmp->arg[i], " \t\n") + 1);
-			in++;
-		}
-		if (tmp->arg[i])
-		{
-			splitted_redirections->arg[i] = tmp->arg[i];
-			i++;
-		}
-	}
-	while (tmp && tmp->arg[i] && in_quote == TRUE)
-	{
-		if (tmp->arg[i] == '\''
-			|| tmp->arg[i] == '"')
-		{
-			if (in == 1)
-				in_quote = FALSE;
-			else
-			{
-				in++;
-				if (tmp->arg[i] == '"')
-				{
-					splitted_redirections->arg = malloc(
-							ft_strcspn(&tmp->arg[i + 1], "\""));
-				}
-				else
-				{
-					splitted_redirections->arg = malloc(
-							ft_strcspn(&tmp->arg[i + 1], "'"));
-				}
-			}
-		}
-		if (tmp->arg[i])
-		{
-			splitted_redirections->arg[i] = tmp->arg[i];
-			i++;
-		}
-	}
-	splitted_redirections->arg[i] = 0;
-	splitted_redirections->e_type = redirection_to_split->e_type;
-	ft_redirection_to_expand_addback(redirs, splitted_redirections);
-	if (tmp->arg[i] == 0)
-	{
-		origin = origin->next;
-		redirection_to_split = redirection_to_split->next;
-		i = 0;
-	}
-	else
-	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(origin->arg, 2);
-		ft_putstr_fd(": ambiguous redirect\n", 2);
-		ft_lstclear_redirections(redirs);
-		return (0);
-	}
-	if (redirection_to_split == NULL)
-		return (0);
-	return (1);
-}
+// 	splitted_redirections = lst_new_redirection_parsing_result();
+// 	if (!splitted_redirections || !redirection_to_split)
+// 		return (0);
+// 	in = 0;
+// 	tmp = redirection_to_split;
+// 	if (tmp->arg[0] == '"'
+// 		|| tmp->arg[0] == '\'')
+// 		in_quote = TRUE;
+// 	else
+// 		in_quote = FALSE;
+// 	while (tmp && tmp->arg[i] && in_quote == FALSE)
+// 	{
+// 		if (tmp->arg[i] == SPACE
+// 			|| tmp->arg[i] == TAB
+// 			|| tmp->arg[i] == NEW_LINE)
+// 			break ;
+// 		else if (in == 0)
+// 		{
+// 			splitted_redirections->arg = malloc(
+// 					ft_strcspn(&tmp->arg[i], " \t\n") + 1);
+// 			in++;
+// 		}
+// 		if (tmp->arg[i])
+// 		{
+// 			splitted_redirections->arg[i] = tmp->arg[i];
+// 			i++;
+// 		}
+// 	}
+// 	while (tmp && tmp->arg[i] && in_quote == TRUE)
+// 	{
+// 		if (tmp->arg[i] == '\''
+// 			|| tmp->arg[i] == '"')
+// 		{
+// 			if (in == 1)
+// 				in_quote = FALSE;
+// 			else
+// 			{
+// 				in++;
+// 				if (tmp->arg[i] == '"')
+// 				{
+// 					splitted_redirections->arg = malloc(
+// 							ft_strcspn(&tmp->arg[i + 1], "\""));
+// 				}
+// 				else
+// 				{
+// 					splitted_redirections->arg = malloc(
+// 							ft_strcspn(&tmp->arg[i + 1], "'"));
+// 				}
+// 			}
+// 		}
+// 		if (tmp->arg[i])
+// 		{
+// 			splitted_redirections->arg[i] = tmp->arg[i];
+// 			i++;
+// 		}
+// 	}
+// 	splitted_redirections->arg[i] = 0;
+// 	splitted_redirections->e_type = redirection_to_split->e_type;
+// 	ft_redirection_to_expand_addback(redirs, splitted_redirections);
+// 	if (tmp->arg[i] == 0)
+// 	{
+// 		origin = origin->next;
+// 		redirection_to_split = redirection_to_split->next;
+// 		i = 0;
+// 	}
+// 	else
+// 	{
+// 		ft_putstr_fd("minishell: ", 2);
+// 		ft_putstr_fd(origin->arg, 2);
+// 		ft_putstr_fd(": ambiguous redirect\n", 2);
+// 		ft_lstclear_redirections(redirs);
+// 		return (0);
+// 	}
+// 	if (redirection_to_split == NULL)
+// 		return (0);
+// 	return (1);
+// }
 
 char	*ft_remove_simple_quote(char *src)
 {
@@ -247,36 +247,147 @@ char	*ft_remove_simple_quote(char *src)
 // Pas de expand sur les heredoc
 
 
+// t_redirection_to_expand	*ft_expand_redirections(t_redirection_to_expand *redirection,
+// 		t_env *env)
+// {
+// 	t_redirection_to_expand	*expand_redirections;
+// 	t_redirection_to_expand	*splitted_redirections;
+// 	t_redirection_to_expand	*tmp_split;
+// 	int						tmp;
+	
+// 	//redirection = NULL;
+// 	tmp = 1;
+// 	expand_redirections = NULL;
+// 	splitted_redirections = NULL;
+// 	tmp_split = NULL;
+// 	expand_redirections = expand_redirection(redirection, env);
+// 	tmp_split = expand_redirections;
+// 	if (expand_redirections)
+// 	{
+// 		while (tmp != 0)
+// 		{
+// 			tmp = ft_split_redirection(tmp_split,
+// 					&splitted_redirections, redirection);
+// 		}
+// 	}
+// 	tmp_split = splitted_redirections;
+// 	while (tmp_split)
+// 	{
+// 		if (tmp_split->arg[0] == '\'' || tmp_split->arg[0] == '"')
+// 			tmp_split->arg = ft_remove_simple_quote(tmp_split->arg);
+// 		tmp_split = tmp_split->next;
+// 	}
+// 	ft_lstclear_redirections(&expand_redirections);
+// 	return (splitted_redirections);
+// }
+
+// Rework redirection parsing in progress !
+
+/*
+* expand_redirection(redirection, env);
+* Si des simples quotes sont trouvées alors traiter tel que
+* des caractères ordinaires quoi qu'il en soit.
+*
+* Faire une fonction qui cherche si un expand doit être fait,
+* de cette manière l'étape précédente n'est plus nécessaire.
+*
+* Expand toutes les redirections.
+* Si l'expand existe pas alors on le remplace par '/0' sauf
+* si la variable a un nom incorrect !
+*/
+
+static void	refresh_quotes_status(t_bool *in_quote, char current)
+{
+	if (in_quote == TRUE && current == '\'')
+		in_quote = FALSE;
+	else if (in_quote == FALSE && current == '\'')
+		in_quote = TRUE;
+}
+
+static int	need_to_be_expand(t_redirection_to_expand *redirection,
+	t_env *env)
+{
+	int		i;
+	t_bool	in_quote;
+	char	*var_name;
+
+	i = 0;
+	in_quote = FALSE;
+	while (redirection->arg && redirection->arg[i])
+	{
+		refresh_quotes_status(&in_quote, redirection->arg[i]);
+		if (redirection->arg[i] == '$' && in_quote == FALSE)
+		{
+			var_name = get_var_name(&redirection->arg[i]);
+			if (lst_search_env(var_name, env))
+			{
+				free (var_name);
+				return (TRUE);
+			}
+			else
+			{
+				free (var_name);
+				return (2);
+			}
+		}
+		i++;
+	}
+	return (FALSE);
+}
+
+t_redirection_to_expand	*expand_redirection(t_redirection_to_expand *redirect, t_env *env)
+{
+	int						i;
+	int						j;
+	char					*var_name;
+	t_redirection_to_expand	*final;
+	t_redirection_to_expand	*tmp;
+
+	var_name = NULL;
+	final = NULL;
+	while (redirect)
+	{
+		i = 0;
+		j = 0;
+		tmp = lst_new_redirection_parsing_result();
+		if (!tmp)
+			return (NULL);
+		tmp->arg = malloc(sizeof(char) * ft_strlen_ultime(
+					redirect, env) + 1);
+		tmp->e_type = redirect->e_type;
+		
+
+
+
+		ft_redirection_to_expand_addback(&final, tmp);
+		redirect = redirect->next;
+	}
+	return (final);
+}
+
 t_redirection_to_expand	*ft_expand_redirections(t_redirection_to_expand *redirection,
 		t_env *env)
 {
 	t_redirection_to_expand	*expand_redirections;
-	t_redirection_to_expand	*splitted_redirections;
-	t_redirection_to_expand	*tmp_split;
-	int						tmp;
-	
+	t_redirection_to_expand	*final_redirections;
+	t_redirection_to_expand	*tmp;
+
 	//redirection = NULL;
 	tmp = 1;
 	expand_redirections = NULL;
-	splitted_redirections = NULL;
-	tmp_split = NULL;
+	final_redirections = NULL;
 	expand_redirections = expand_redirection(redirection, env);
-	tmp_split = expand_redirections;
 	if (expand_redirections)
 	{
-		while (tmp != 0)
+		tmp = expand_redirections;
+		while (tmp)
 		{
-			tmp = ft_split_redirection(tmp_split,
-					&splitted_redirections, redirection);
+			// Verif si y a des IFS
+			if (tmp->arg[0] == '\'' || tmp->arg[0] == '"')
+				tmp->arg = ft_remove_simple_quote(tmp->arg);
+			tmp = tmp->next;
 		}
 	}
-	tmp_split = splitted_redirections;
-	while (tmp_split)
-	{
-		if (tmp_split->arg[0] == '\'' || tmp_split->arg[0] == '"')
-			tmp_split->arg = ft_remove_simple_quote(tmp_split->arg);
-		tmp_split = tmp_split->next;
-	}
 	ft_lstclear_redirections(&expand_redirections);
-	return (splitted_redirections);
+	return (final_redirections);
 }
