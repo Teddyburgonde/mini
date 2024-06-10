907b3b970ec75c1153b1b0196fcdcf3961b887ce
Romain <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-06-10 12:23:20 +0200

    Add files via upload

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,32 +6,37 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:45:02 by rgobet            #+#    #+#             */
-/*   Updated: 2024/05/31 12:45:10 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/06/10 12:11:15 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	unset(t_env **env, char *name)
+void	unset(t_env **env, char **names)
 {
+	int		i;
 	t_env	*tmp;
 	t_env	*prev;
 
-	tmp = *env;
-	prev = *env;
+	i = 1;
 	//exit code 1 si cas d'erreur 
-	while (tmp)
+	while (names[i])
 	{
-		if (ft_strcmp(tmp->var_name, name) == 0)
+		tmp = *env;
+		prev = *env;
+		while (tmp)
 		{
-			prev->next = tmp->next;
-			free(tmp->var_name);
-			free(tmp->full_path);
-			free(tmp->value);
-			free(tmp);
-			break ;
+			if (ft_strcmp(tmp->var_name, names[i]) == 0)
+			{
+				prev->next = tmp->next;
+				free(tmp->var_name);
+				free(tmp->full_path);
+				free(tmp->value);
+				free(tmp);
+				break ;
+			}
+			prev = tmp;
+			tmp = tmp->next;
 		}
-		prev = tmp;
-		tmp = tmp->next;
 	}
 }
