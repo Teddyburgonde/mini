b319e8a1f06ee7f00e73fe5dd23633959cc7f4b1
Romain <140979120+Romain-ItAllDepends@users.noreply.github.com>
2024-06-15 15:12:09 +0200

    Add files via upload

--- src/exec/heredoc.c
+++ src/exec/heredoc.c
@@ -6,50 +6,12 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:11:14 by tebandam          #+#    #+#             */
-/*   Updated: 2024/06/12 14:32:05 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/06/15 15:10:10 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../minishell.h"
 #include <unistd.h>
-
-// void	ft_get_nb_hd(t_vars *vars, t_redirection_to_expand *redir)
-// {
-// 	int	count;
-
-// 	count = 0;
-// 	while (redir)
-// 	{
-// 		if (redir->e_type == REDIRECTION_HEREDOC)
-// 			count++;
-// 		redir = redir->next;
-// 	}
-// 	vars->nb_hd = count;
-// }
-
-// void	ft_heredoc(t_vars *vars, t_redirection_to_expand *redir)
-// {
-// 	char	*tab;
-// 	int		i;
-
-// 	i = 0;
-// 	tab = NULL;
-// 	ft_get_nb_hd(vars, redir);
-// 	open_hd_w(vars);
-// 	while (redir && vars->nb_hd > 0)
-// 	{
-// 		while (redir->e_type != REDIRECTION_HEREDOC)
-// 			redir = redir->next;
-// 		write(1, "> ", 2);
-// 		tab = get_next_line(STDIN_FILENO);
-// 		verif_tab(tab);
-// 		capture_and_redirection(tab, redir->arg, vars);
-// 		redir = redir->next;
-// 		vars->nb_hd--;
-// 	}
-// 	close(vars->hd_w);
-// 	open_fd_infile(vars);
-// }
 
 void	fill_tmp_content(char **tmp_content, t_redirection *redirection, int fd_tmp)
 {
@@ -59,7 +21,8 @@ void	fill_tmp_content(char **tmp_content, t_redirection *redirection, int fd_tmp
 	*tmp_content = readline("> ");
 	if (*tmp_content == NULL)
 	{
-		perror("Error readline\n");
+		free(*tmp_content);
+		ft_putstr_fd("bash: warning: here-document at line 5 delimited by end-of-file (wanted `:')", 2);
 		return ;
 	}
 	if (ft_strcmp(*tmp_content, tmp_redirection->limiter) == 0)
@@ -76,16 +39,6 @@ void	fill_tmp_content(char **tmp_content, t_redirection *redirection, int fd_tmp
 		ft_putstr_fd("\n", fd_tmp);
 	}
 }
-
-// void	open_fd_tmp_for_heredoc(int *fd_tmp)
-// {
-// 	*fd_tmp = open("tmp_heredoc", O_TRUNC | O_CREAT | O_WRONLY, 0644);
-// 	if (*fd_tmp < 0)
-// 	{
-// 		perror("Error opening file\n");
-// 		return ;
-// 	}
-// }
 
 void	ft_heredoc(t_redirection *redirection,
 		t_redirection_to_expand *all, t_bool save)
@@ -122,6 +75,12 @@ void	ft_heredoc(t_redirection *redirection,
 						free(tmp_content);
 					tmp_content = NULL;
 					tmp_content = readline("> ");
+					if (tmp_content == NULL)
+					{
+						free(tmp_content);
+						ft_putstr_fd("bash: warning: here-document at line 5 delimited by end-of-file (wanted `:')", 2);
+						break ;
+					}
 				}
 				count++;
 			}
